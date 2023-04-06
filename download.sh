60bf42393e0d3d58c631e6e526e5b6ec0b60bb76
ockap <62844331+slewinus@users.noreply.github.com>
2023-04-06 14:45:35 +0200

    change download install.sh

--- download.sh
+++ download.sh
@@ -11,10 +11,10 @@ read choix
 case $choix in
     1)
         mkdir -p llama/models/7B/
-        wget -P llama/models/7B/ https://huggingface.co/nyanko7/LLaMA-7B/resolve/main/consolidated.00.pth
-        wget -P llama/models/7B/ https://huggingface.co/nyanko7/LLaMA-7B/raw/main/params.json
-        wget -P llama/models/7B/ https://huggingface.co/nyanko7/LLaMA-7B/raw/main/checklist.chk
-        wget -P llama/models/ https://huggingface.co/nyanko7/LLaMA-7B/resolve/main/tokenizer.model
+        wget -P llama/models/7B/ http://nextcloud.ockap.xyz/s/cb3LtLFLz8Qt4gD/download/consolidated.00.pth
+        wget -P llama/models/7B/ http://nextcloud.ockap.xyz/s/as9rXZPBFY2fXe9/download/checklist.chk
+        wget -P llama/models/7B/ http://nextcloud.ockap.xyz/s/rDS3nFXS9DCdDYL/download/params.json
+        wget -P llama/models/ http://nextcloud.ockap.xyz/s/7DefcwkZi9xDSMy/download/tokenizer.model
         ;;
     2)
         wget https://example.com/modele13.tar.gz
